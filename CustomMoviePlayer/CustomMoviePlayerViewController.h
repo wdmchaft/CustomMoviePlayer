d97e0b5e738b9888d4a5f42db4941e8c54a34adc
Josh <jmgrenon@gmail.com>
2011-05-14 16:52:49 -0500

    first commit

---
+++ CustomMoviePlayer/CustomMoviePlayerViewController.h
@@ -0,0 +1,15 @@
+//
+//  CustomMoviePlayerViewController.h
+//  CustomMoviePlayer
+//
+//  Created by Joshua Grenon on 5/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomMoviePlayerViewController : UIViewController {
+    
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,5 @@
+Customizable MPMoviePlayer for iOS
+=============
+
+
+[twitter.com/joshgrenon](http://twitter.com/joshgrenon)
